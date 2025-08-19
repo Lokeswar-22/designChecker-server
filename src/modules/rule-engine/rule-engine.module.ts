8d04db5bedb915b43428123c2b5d9ca72ef2f80b
Harish <harish.n@krionconsulting.com>
2025-08-19 22:25:43 +0530

    Added TypeOrmModule for Issue entity and included IssueService and RequestService in RuleEngineModule providers.

--- src/modules/rule-engine/rule-engine.module.ts
+++ src/modules/rule-engine/rule-engine.module.ts
@@ -3,10 +3,14 @@ import { RuleEngineService } from './rule-engine.service';
 import { RuleEngineController } from './rule-engine.controller';
 import { ACCAuthModule } from '../acc-auth/acc-auth.module';
 import { HttpModule } from '@nestjs/axios';
+import { IssueService } from 'src/shared/services/issue.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Issue } from 'src/shared/entities/issue.entity';
+import { RequestService } from 'src/shared/services/request.service';
 
 @Module({
-  imports: [ACCAuthModule,HttpModule],
+  imports: [ACCAuthModule,HttpModule,TypeOrmModule.forFeature([Issue])],
   controllers: [RuleEngineController],
-  providers: [RuleEngineService]
+  providers: [RuleEngineService, IssueService,RequestService]
 })
 export class RuleEngineModule {}
